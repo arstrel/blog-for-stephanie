0a51dd885fb42cceefdbc87d91d28192408507c9
Artem <arstrel@gmail.com>
2022-10-29 15:21:59 -0700

    updates

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  pageLoadTimeout: 10000,
   e2e: {
     baseUrl: 'http://localhost:3000',
     supportFile: false,
